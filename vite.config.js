5442da5b9af3899990b976152153d57ec84fba95
Prerna Lele <prernalele@gmail.com>
2025-05-31 21:56:37 -0400

    feat: adding back the base in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: "/add-to-cart/",
+  base: "/add-to-cart/",
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
