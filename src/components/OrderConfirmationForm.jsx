0ea390757ae217a3cbd755e9ccccffa1c4c6faf0
Prerna Lele <prernalele@gmail.com>
2025-07-06 13:49:29 -0400

    style: fixes height of confirmation window

--- src/components/OrderConfirmationForm.jsx
+++ src/components/OrderConfirmationForm.jsx
@@ -5,7 +5,7 @@ import ListOfConfirmedItems from "./listOfConfirmedItems";
 const OrderConfirmationForm = ({ itemsInCart, setShowOrderConfirmed }) => {
   return (
     <div className=" fixed inset-0 z-50 bg-black/50 flex flex-col items-center justify-center  ">
-      <div className="bg-rose-50 h-[50%] w-[30%] justify-around rounded-2xl min-h-dvh">
+      <div className="bg-rose-50 justify-around rounded-2xl  min-h-fit">
         <img src={iconOrderConfirmed} className="size-10 ml-2 mt-2" />
         <h2 className="font-bold text-2xl text-black ml-2 mt-2">
           Order Confirmed
@@ -15,7 +15,7 @@ const OrderConfirmationForm = ({ itemsInCart, setShowOrderConfirmed }) => {
         </h5>
         <ListOfConfirmedItems itemsInCart={itemsInCart} />
         <button
-          className="bg-rose-200 text-black justify-center text-xl p-2 m-2 rounded-xl font-medium hover:bg-rose-300 hover:transition-300"
+          className="bg-rose-200 text-black justify-center text-xl p-2 m-2 rounded-xl font-medium hover:bg-rose-300 hover:transition-300 w-[90%]"
           onClick={() => setShowOrderConfirmed(false)}
         >
           Start new order
