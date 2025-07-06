6314aca5d06667d6d956aa108d276a2fc392f9ea
Prerna Lele <prernalele@gmail.com>
2025-07-06 10:16:10 -0400

    feat: adds list of cart items to confirmation window

---
+++ src/components/ListOfConfirmedItems.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const ListOfConfirmedItems = ({ itemsInCart }) => {
+  console.log("items in cart", itemsInCart);
+  return (
+    <div>
+      {itemsInCart.map((item, index) => {
+        const { image, name, price, quantity } = item;
+        console.log("item", item);
+        const { thumbnail } = image;
+        return (
+          <div className="flex flex-row justify-around p-2 m-2 grow-2 bg-rose-100">
+            <img
+              src={thumbnail}
+              className="size-15 rounded-md flex-shrink-0 "
+            />
+            <div className="flex flex-col justify-between flex-1 min-w-0">
+              <div>{name}</div>
+              <div className="flex flex-row justify-items-center m-2">
+                <span className="m-2">{`${quantity}x`}</span>
+                <span className="m-2">{`${price}`}</span>
+              </div>
+            </div>
+            <div className="flex justify-center w-20 align-middle">
+              {`$${quantity * price}`}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ListOfConfirmedItems;
--- src/components/OrderConfirmationForm.jsx
+++ src/components/OrderConfirmationForm.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import iconOrderConfirmed from "/assets/images/icon-order-confirmed.svg";
-const OrderConfirmationForm = ({ setShowOrderConfirmed }) => {
+import ListOfConfirmedItems from "./listOfConfirmedItems";
+
+const OrderConfirmationForm = ({ itemsInCart, setShowOrderConfirmed }) => {
   return (
-    <div className=" fixed inset-0 z-50 bg-black/50 flex flex-col items-center justify-center ">
-      <div className="bg-amber-50 h-[50%] w-[30%] justify-around rounded-2xl">
-        <img src={iconOrderConfirmed} />
-        <h2 className="font-bold text-2xl text-black">Order Confirmed</h2>
-        <h5>We hope you enjoy your food</h5>
+    <div className=" fixed inset-0 z-50 bg-black/50 flex flex-col items-center justify-center  ">
+      <div className="bg-rose-50 h-[50%] w-[30%] justify-around rounded-2xl min-h-dvh">
+        <img src={iconOrderConfirmed} className="size-10 ml-2 mt-2" />
+        <h2 className="font-bold text-2xl text-black ml-2 mt-2">
+          Order Confirmed
+        </h2>
+        <h5 className="text-rose-500 font-light ml-3">
+          We hope you enjoy your food
+        </h5>
+        <ListOfConfirmedItems itemsInCart={itemsInCart} />
         <button
-          className="bg-rose-200 text-black justify-center text-xl p-2 rounded-xl font-medium hover:bg-rose-300 hover:transition-300"
+          className="bg-rose-200 text-black justify-center text-xl p-2 m-2 rounded-xl font-medium hover:bg-rose-300 hover:transition-300"
           onClick={() => setShowOrderConfirmed(false)}
         >
           Start new order
